f0a13dcdd0935a34a722afb1129766760c8435fb
Nobu <hiyodev@gmail.com>
2023-06-17 21:25:46 +0800

    Changed to JS for simplicity

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import QuizMenuPage from "./pages/QuizMenuPage";
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <QuizMenuPage />
+    </Container>
+  );
+}
+
+export default App;
