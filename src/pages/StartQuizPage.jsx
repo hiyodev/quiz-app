387d1d99b510e3cca91a5205b503a60d485b2712
Nobu <hiyodev@gmail.com>
2023-06-27 21:27:50 +0800

    Added Start Quiz Page
    WIP

---
+++ src/pages/StartQuizPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// TODO: This will be the content for when user start quizzes
+// Add a progressive timer bar at the top, probably in the Navbar
+// Change the Navbar title to the quiz's card name
+// Remove the theme switch on the right, we don't want users changing theme and being distracted while doing the quiz
+function StartQuizPage(props) {
+  return <div></div>;
+}
+
+export default StartQuizPage;
